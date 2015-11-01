d4b67003e70f1899369ba71c51194abcdfbe547d
dusan <dusan@localhost.localdomain>
2015-11-01 12:11:08 +0100

    Lexical analyzator:
    - comments
    	-block
    	-in line
    - identificators
    -non negative number
    	-int
    	-double
    	-float
    	-event with exponencial part
    -declaration of variable in format:
    	-int double;

--- parser.c
+++ parser.c
@@ -8,11 +8,13 @@ enum e_parser_state {PS_DEFAULT,
                     PS_SLASH,
                     PS_BLOCK_COMMENT,
                     PS_IDENTIFICATOR,
-                    PS_INT_PART,
+                    PS_INT_PART_1,
+                    PS_INT_PART_2,
                     PS_FRACTIONAL_PART,
                     PS_EXPONENCIAL_PART,
                     PS_WHITESPACE,
                     PS_FIRST_WHITESPACE,
+                    PS_IDENTIFICATOR_OF_DECLARATION,
                     PS_ARGUMENTS_OF_DECLARATIONS_START,
                     PS_ARGUMENTS_OF_DECLARATIONS_INSIDE};
 
@@ -44,21 +46,25 @@ int parser_next_token()
         case PS_DEFAULT:
 
             //default state
+
+            //eats whitespace
             if (c == '\n' || c == ' ' || c == '\t')
                 state = PS_DEFAULT;
 
+            //start od commentar
             else if(c== '/')
                 state = PS_SLASH;
 
+            //start of identificator
             else if( c == '_' || ( c > 64 && c < 91) || ( c > 96 && c < 123) )
             {
                 ungetc(c, source);
                 state = PS_IDENTIFICATOR;
             }
 
-
+            //start of non negative number
             else if ( c > 47 && c < 58)
-                state = PS_INT_PART;
+                state = PS_INT_PART_1;
 
 
             break;
@@ -92,12 +98,18 @@ int parser_next_token()
         case PS_BLOCK_COMMENT:
 
             if (c == '/')
+            {
                 if (c_before == '*')
                     state = PS_DEFAULT;
+
+            }
+            else
+                state = PS_BLOCK_COMMENT;
 
             break;
 
         case PS_IDENTIFICATOR:
+
 
             if ( c == '_' || ( c > 64 && c < 91) || ( c > 96 && c < 123) || ( c > 47 && c < 58) )
                 state = PS_IDENTIFICATOR;
@@ -105,54 +117,87 @@ int parser_next_token()
             else
             {
                     ungetc(c, source);
-                    if(parser_control_type(s) != 0)
+                    if(parser_control_type(s) == 0)
                     {
-                       //Identificator is not TYPE
-                        if(type_read == true){
-                            state = PS_ARGUMENTS_OF_DECLARATIONS_START;
-                            break;
-                        }
-                        state = PS_DEFAULT;
-                    }
-                    else
-                    {
-                        if (type_read == true)
-                        {
-                            error("Type keyword can't be followed by another type keyword.", ERROR_LEX);
-
-                        }
-                        type_read = true;
+                        //We found keyword
+                        //do something wit keyword
                         state = PS_FIRST_WHITESPACE;
+
                     }
+
+                    //do something with identificator
+                    state = PS_DEFAULT;
             }
 
 
             break;
 
-        case PS_INT_PART:
+        case PS_INT_PART_1:
 
-            if(c_before == '0' && c != '.')
-                error("Incorrect representation of number", ERROR_LEX);
+            if(c_before == '0')
+            {
+                if (c == '.')
+                {
+                    state = PS_FRACTIONAL_PART;
+                }
+                else
+                    error("Incorrect representation of number", ERROR_LEX);
+            }
+            else if (c >= '1' && c <= '9')
+                {
+                    //spracuj znak
+                    state = PS_INT_PART_2;
+                }
 
-            if (c == '.')
+            else if (c == '.')
                 state = PS_FRACTIONAL_PART;
-            else if (c > 47 && c < 58)
-                state = PS_INT_PART;
+
 
 
             else{
 
                 ungetc(c, source);
+                state = PS_DEFAULT;
             }
+            break;
+
+        case PS_INT_PART_2:
+
+            if (c >= '1' && c <= '9')
+            {
+                //spracuj
+                state = PS_INT_PART_2;
+            }
+            else if (c == '.')
+            {
+                //spracuj
+                state = PS_FRACTIONAL_PART;
+            }
+            else if (c == 'E' || c == 'e')
+            {
+                //spracuj
+                state = PS_EXPONENCIAL_PART;
+            }
+            else
+            {
+                //spracuj
+                ungetc(c, soubor);
+                state = PS_DEFAULT;
+
+            }
+            break;
+
 
         case PS_FRACTIONAL_PART:
 
             if( c > 47 && c < 58 )
+                //spracuj
                 //pokracujem v citani znakov
                 state = PS_FRACTIONAL_PART;
 
-            else if (c == 'E' || c == 'e'){
-
+            else if (c == 'E' || c == 'e')
+            {
+                //spracuj
                 state = PS_EXPONENCIAL_PART;
             }
             else if (c == '.')
@@ -164,28 +209,39 @@ int parser_next_token()
 
         case PS_EXPONENCIAL_PART:
 
-            //iam in this state for the first time
+
             if (c_before == 'E' || c_before == 'e' )
+            //first time in this state
             {
                 if(c == '+' || c == '-')
+                {
+                    //spracuj
+                    state = PS_EXPONENCIAL_PART;
+                }
 
-                    TODO;
 
-                else if(c > 47 && c < 58)
+                else if(c >= '0' && c <= '9')
 
                     TODO;
                 else if(c == '.')
-                    //ERROR
-                    TODO;
+                    error("Incorect representation of number", ERROR_LEX);
             }
 
-            else if(c > 47 && c < 58){
+            else if(c >= '0' && c <= '9'){
 
-                TODO;
+                //spracuj
+                state = PS_EXPONENCIAL_PART;
             }
 
             else if(c == '.')
                 error("Incorrect representation of number", ERROR_LEX);
+
+            else
+            {
+                ungetc(c, soubor);
+                //send number to token
+                state = PS_DEFAULT;
+            }
 
 
             break;
@@ -209,7 +265,7 @@ int parser_next_token()
 
             else
             {
-                state = PS_IDENTIFICATOR;
+                state = PS_IDENTIFICATOR_OF_DECLARATION;
             }
             break;
 
@@ -246,6 +302,39 @@ int parser_next_token()
 
             break;
 
+        case PS_IDENTIFICATOR_OF_DECLARATION:
+
+            if ( c == '_' || ( c > 64 && c < 91) || ( c > 96 && c < 123) || ( c > 47 && c < 58) )
+            {
+                state = PS_IDENTIFICATOR_OF_DECLARATION;
+                //save char in string
+            }
+            else if(c_before == '_' || ( c_before > 64 && c_before < 91) || ( c_before > 96 && c_before < 123) )
+            {   //first time in this state, keyword of type isn't followed by identificator
+                error("Keyword type without identificator of variable.");
+
+            }
+            else
+            {
+                ungetc(c, source);
+                if(parser_control_type(s) == 0)
+                {
+                    //Identificator is keyword
+
+                    error("Type keyword can't be followed by another type keyword.", ERROR_LEX);
+
+                }
+                else
+                {
+
+                    state = PS_ARGUMENTS_OF_DECLARATIONS_START;
+                }
+
+                }
+            }
+
+
+
         }
 
     }
@@ -253,6 +342,9 @@ int parser_next_token()
 
 }
 
+/************************************
+ * check for keywords of language
+ ************************************/
 int parser_control_type(str *s){
 
     if (str_equals(s, "double"))
